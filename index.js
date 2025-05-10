5047ef26b246a58573f462d1f1867450bc503ae9
abhaydesai01 <70378731+abhaydesai01@users.noreply.github.com>
2025-05-10 20:20:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var express=require('express')
-var bodyParser=require('body-parser')
-var mysql=require('mysql')
-var app=express()
-app.use(bodyParser())
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-app.get("/",function(request,response){
-  response.send("hello from express js....")
+const app = express();
+
+// Use JSON and URL-encoded parsers individually
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express.js....");
 });
 
-
-app.get("/date",function(request,response){
-  response.send("Date is:.."+new Date().toLocaleString())
+app.get("/date", (req, res) => {
+  res.send("Date is: " + new Date().toLocaleString());
 });
 
-app.listen(9595,function(err){
-  if (err) throw err
-  console.log("Server started on port 9595, to shut down hit ctrl+c")
-})
+app.listen(9595, '0.0.0.0', (err) => {
+  if (err) throw err;
+  console.log("Server started on port 9595");
+});
